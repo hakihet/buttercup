cec321dc89db8b5c4cb1a64cc7b024fd6f1e2d91
me <pickleslicesforall@outlook.com>
2022-07-04 14:56:44 -0400

    trunk

--- buttercup.go
+++ buttercup.go
@@ -3,11 +3,10 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"time"
 )
 
 type Buttercup struct {
@@ -21,43 +20,37 @@ func (*Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not Implemented"))
 }
 
-func shutdown() (err error) {
-	c, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	return server.Shutdown(c)
-}
-
 func main() {
-	var status int
 
+	var crt = flag.String("crt", "", "cert")
+	var key = flag.String("key", "", "key")
 	flag.Parse()
 
-	server := http.Server{
-		Addr:              ":8080",
-		Handler:           new(Buttercup),
-		ReadTimeout:       10 * time.Second,
-		ReadHeaderTimeout: 0,
-		WriteTimeout:      0,
-		IdleTimeout:       0,
-		MaxHeaderBytes:    0,
+	var s http.Server = http.Server{
+		Addr:              ":https",
+		Handler:           &Buttercup{},
+		ReadTimeout:       1,
+		ReadHeaderTimeout: 1,
+		IdleTimeout:       1,
 	}
 
-	go func(errchan chan error) {
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
-			errchan <- err
-		}
-	}(errchan)
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
-	select {
-	case v := <-err:
-		status = 1
-	case <-signals:
-
+	done := func() <-chan struct{} {
+		d := make(chan struct{})
+		go func(c chan struct{}) {
+			defer close(c)
+			signals := make(chan os.Signal, 1)
+			signal.Notify(signals, os.Interrupt)
+			<-signals
+			if err := s.Shutdown(context.Background()); err != nil {
+				log.Printf("HTTP server Shutdown: %v", err)
+			}
+		}(d)
+		return d
 	}
 
-	shutdown()
-	os.Exit(status)
+	if err := s.ListenAndServeTLS(*crt, *key); err != http.ErrServerClosed {
+		log.Printf("HTTP server ListenAndServe: %v", err)
+		return
+	}
+	<-done()
 }
